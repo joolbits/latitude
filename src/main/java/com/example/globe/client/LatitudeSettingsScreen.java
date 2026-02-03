e1025811e328f587a8aacce156eb1ea4bb35c126
Peetsa <225305569+peetsamods@users.noreply.github.com>
2026-02-02 22:36:54 -0500

    compat(1.21.1): fix SettingsScreen CyclingButtonWidget builder API

--- src/main/java/com/example/globe/client/LatitudeSettingsScreen.java
+++ src/main/java/com/example/globe/client/LatitudeSettingsScreen.java
@@ -43,16 +43,18 @@ public class LatitudeSettingsScreen extends Screen {
         int baseY;
 
         baseY = y;
-        var wZoneTitle = this.addDrawableChild(CyclingButtonWidget.builder(v -> Text.literal(v ? "ON" : "OFF"), LatitudeConfig.zoneEnterTitleEnabled)
+        var wZoneTitle = this.addDrawableChild(CyclingButtonWidget.<Boolean>builder(v -> Text.literal(v ? "ON" : "OFF"))
                 .values(true, false)
+                .initially(LatitudeConfig.zoneEnterTitleEnabled)
                 .build(columnX, y, w, h, Text.literal("Zone Enter Title"), (btn, value) -> LatitudeConfig.zoneEnterTitleEnabled = value));
         layoutWidgets.add(wZoneTitle);
         layoutBaseYs.add(baseY);
         y += 24;
 
         baseY = y;
-        var wWarnText = this.addDrawableChild(CyclingButtonWidget.builder(v -> Text.literal(v ? "ON" : "OFF"), LatitudeConfig.showWarningMessages)
+        var wWarnText = this.addDrawableChild(CyclingButtonWidget.<Boolean>builder(v -> Text.literal(v ? "ON" : "OFF"))
                 .values(true, false)
+                .initially(LatitudeConfig.showWarningMessages)
                 .build(columnX, y, w, h, Text.literal("Warning messages"), (btn, value) -> {
                     LatitudeConfig.showWarningMessages = value;
                     LatitudeConfig.saveCurrent();
@@ -69,8 +71,9 @@ public class LatitudeSettingsScreen extends Screen {
         y += 24;
 
         baseY = y;
-        var wShowMode = this.addDrawableChild(CyclingButtonWidget.<CompassHudConfig.ShowMode>builder(this::showModeLabel, () -> cfg.showMode)
+        var wShowMode = this.addDrawableChild(CyclingButtonWidget.<CompassHudConfig.ShowMode>builder(this::showModeLabel)
                 .values(CompassHudConfig.ShowMode.values())
+                .initially(cfg.showMode)
                 .build(columnX, y, w, h, Text.literal("Show Mode"), (btn, value) -> cfg.showMode = value));
         layoutWidgets.add(wShowMode);
         layoutBaseYs.add(baseY);
